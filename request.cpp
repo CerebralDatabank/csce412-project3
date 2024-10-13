ac6ff44d40e3359bc90952db42698259878e1dab
Gopal Othayoth <cerebraldatabank@tamu.edu>
2024-10-13 17:13:27 -0500

    Add Request code

--- request.cpp
+++ request.cpp
@@ -1,3 +1,22 @@
 #include "request.h"
 
-Request::Request() {}
+string Request::randIp() {
+    string ip = "";
+    for (int i = 0; i < 4; i++) {
+        ip += to_string(rand() % 256);
+        if (i < 3) ip += '.';
+    }
+    return ip;
+}
+
+Request::Request() :
+    ipIn{randIp()},
+    ipOut{randIp()},
+    requiredTime{rand() % 1000 + 1uLL}
+{}
+
+Request::Request(string ipIn, string ipOut, uint64_t requiredTime) :
+    ipIn{ipIn},
+    ipOut{ipOut},
+    requiredTime{requiredTime}
+{}
--- request.h
+++ request.h
@@ -1,9 +1,18 @@
 #ifndef REQUEST_H
 #define REQUEST_H
 
+#include <string>
+#include "common.h"
+
 class Request {
 public:
+    string ipIn;
+    string ipOut;
+    uint64_t requiredTime;
+
     Request();
+    Request(string ipIn, string ipOut, uint64_t requiredTime);
+    static string randIp();
 };
 
 #endif // REQUEST_H
