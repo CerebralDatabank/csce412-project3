09b78ef80652f1654cc3f21fdacf66a520d6c78d
Gopal Othayoth <cerebraldatabank@tamu.edu>
2024-10-18 11:03:02 -0500

    Add WebServer code

--- web_server.cpp
+++ web_server.cpp
@@ -1,3 +1,19 @@
 #include "web_server.h"
 
-WebServer::WebServer() {}
+WebServer::WebServer(uint32_t id) : request{nullptr}, timeWhenFree{0}, id{id} {}
+
+void WebServer::takeRequest(Request* req, uint64_t time) {
+    if (request != nullptr) {
+        printf(
+            "\e[96m[Server %u]\e[m Request of size %llu (%s -> %s) completed at time %llu\n",
+            id, request->requiredTime, request->ipIn.c_str(), request->ipOut.c_str(), time
+        );
+        delete request;
+    }
+    request = req;
+    timeWhenFree = time + req->requiredTime;
+}
+
+bool WebServer::isFree(uint64_t time) {
+    return time >= timeWhenFree;
+}
--- web_server.h
+++ web_server.h
@@ -1,9 +1,19 @@
 #ifndef WEB_SERVER_H
 #define WEB_SERVER_H
 
+#include "common.h"
+#include "request.h"
+
 class WebServer {
+private:
+    Request* request;
+    uint64_t timeWhenFree;
 public:
-    WebServer();
+    uint32_t id;
+
+    WebServer(uint32_t id);
+    void takeRequest(Request* req, uint64_t time);
+    bool isFree(uint64_t time);
 };
 
 #endif // WEB_SERVER_H
