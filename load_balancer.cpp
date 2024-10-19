1226c0f76ba15b5ec47d4cebd0d9124538efc416
Gopal Othayoth <Gopal.Othayoth@outlook.com>
2024-10-19 14:59:10 -0500

    Add load balancer code

--- load_balancer.cpp
+++ load_balancer.cpp
@@ -1,3 +1,38 @@
 #include "load_balancer.h"
 
-LoadBalancer::LoadBalancer() {}
+LoadBalancer::LoadBalancer(uint32_t maxServers, uint32_t maxTimeDigits) :
+    time{0}, requests{}, servers{}, maxServers{maxServers}
+{
+    logInfo = LogInfo{
+        maxTimeDigits,
+        static_cast<uint32_t>(to_string(maxServers).length()),
+        static_cast<uint32_t>(to_string(REQ_SIZE_MAX).length())
+    };
+    for (uint32_t i = 1; i <= maxServers; ++i)
+        servers.push_back(new WebServer{i, logInfo});
+}
+
+LoadBalancer::~LoadBalancer() {
+    for (; !requests.empty(); delete requests.front(), requests.pop());
+    for (WebServer* server : servers) delete server;
+}
+
+void LoadBalancer::addRequest(Request* request) {
+    requests.push(request);
+}
+
+uint64_t LoadBalancer::clock() {
+    for (WebServer* server : servers) {
+        if (requests.empty()) goto end;
+        if (!server->isFree(time)) continue;
+        Request* request = requests.front();
+        requests.pop();
+        server->takeRequest(request, time);
+    }
+    end:
+    return ++time;
+}
+
+size_t LoadBalancer::queueSize() {
+    return requests.size();
+}
--- load_balancer.h
+++ load_balancer.h
@@ -1,9 +1,25 @@
 #ifndef LOAD_BALANCER_H
 #define LOAD_BALANCER_H
 
+#include "common.h"
+#include "request.h"
+#include "web_server.h"
+#include <queue>
+#include <vector>
+
 class LoadBalancer {
+private:
+    uint64_t time;
+    queue<Request*> requests;
+    vector<WebServer*> servers;
+    uint32_t maxServers;
+    LogInfo logInfo; // to pass to WebServer
 public:
-    LoadBalancer();
+    LoadBalancer(uint32_t maxServers, uint32_t maxTimeDigits);
+    ~LoadBalancer();
+    void addRequest(Request* request);
+    uint64_t clock();
+    size_t queueSize();
 };
 
 #endif // LOAD_BALANCER_H
