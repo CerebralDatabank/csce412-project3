22e2d0fd4eb6ce08b2efcb06bb57874f58535678
Gopal Othayoth <Gopal.Othayoth@outlook.com>
2024-10-19 14:58:38 -0500

    Update web server logging

--- web_server.cpp
+++ web_server.cpp
@@ -1,12 +1,14 @@
 #include "web_server.h"
 
-WebServer::WebServer(uint32_t id) : request{nullptr}, timeWhenFree{0}, id{id} {}
+WebServer::WebServer(uint32_t id, LogInfo logInfo) :
+    request{nullptr}, timeWhenFree{0}, logInfo{logInfo}, id{id}
+{}
 
 void WebServer::takeRequest(Request* req, uint64_t time) {
     if (request != nullptr) {
         printf(
-            "\e[96m[Server %u]\e[m Request of size %llu (%s -> %s) completed at time %llu\n",
-            id, request->requiredTime, request->ipIn.c_str(), request->ipOut.c_str(), time
+            "\e[2m[%*llu] \e[22;96m[Server \e[38;5;220m%*u\e[96m]\e[m Request of size \e[38;5;40m%*llu\e[m (\e[38;5;208m%15s\e[m -> \e[38;5;207m%-15s\e[m) completed\n",
+            logInfo.maxTimeDigits, time, logInfo.maxIdDigits, id, logInfo.maxReqSizeDigits, request->requiredTime, request->ipIn.c_str(), request->ipOut.c_str()
         );
         delete request;
     }
--- web_server.h
+++ web_server.h
@@ -4,14 +4,21 @@
 #include "common.h"
 #include "request.h"
 
+struct LogInfo {
+    uint32_t maxTimeDigits;
+    uint32_t maxIdDigits;
+    uint32_t maxReqSizeDigits;
+};
+
 class WebServer {
 private:
     Request* request;
     uint64_t timeWhenFree;
+    LogInfo logInfo; // for logging
 public:
     uint32_t id;
 
-    WebServer(uint32_t id);
+    WebServer(uint32_t id, LogInfo logInfo);
     void takeRequest(Request* req, uint64_t time);
     bool isFree(uint64_t time);
 };
